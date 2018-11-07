cbc9c9c2886c189e3117a1b996e92ce6762b6863
BalgarokIpi <44801537+BalgarokIpi@users.noreply.github.com>
2018-11-07 09:38:02 +0100

    nouvelle fonction
    on utilise la fonction mult

--- test_jenk/src/main/java/test_jenk/essai.java
+++ test_jenk/src/main/java/test_jenk/essai.java
@@ -3,6 +3,10 @@ package test_jenk;
 public class essai {
 
 	public static void main(String[] args){
-		System.out.println("Hello World");
+		//System.out.println("Hello World");
+	}
+	
+	public int mult(int a, int b){
+		return a*b;
 	}
 }
